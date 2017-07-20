dddf8462ac3b07cd13523bbf4a8228baabbe3b75
Iman <imangatine@gmail.com>
2017-07-20 13:58:08 +0200

    login

--- app/index.js
+++ app/index.js
@@ -2,8 +2,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//const IndexCtrl = require('./controllers/IndexCtrl');
+//const IndexCtrl = rgit equire('./controllers/IndexCtrl');
 
+const LoginCtrl = require('./controllers/LoginCtrl');
 const config = {
     name: "Bibi"
 };
@@ -27,22 +28,18 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 //const indexCtrl = new IndexCtrl(config);
 
 // Avec une vue
-app.get('/:name?', (req, res) => {
-    const name = req.params.name || 'inconnu';
-
 //app.get('/:name?', indexCtrl.index.bind(indexCtrl));
 app.get('/registration', (req, res) => {});
-app.get('/login', (req, res) => {});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.get('/generator', (req, res) => {});
 app.get('/download', (req, res) => {});
 app.post('/postRegistration', (req, res) => {});
 app.post('/postLogin', (req, res) => {});
 app.post('/postGenerator', (req, res) => {});
-
-res.render('index', {
-    name
-});
-});
 //app.post('/auth', indexCtrl.auth.bind(indexCtrl));
 
 app.listen(port, () => console.log('Connection ready on : ${port}'));
