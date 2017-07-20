95616f2ddd669bc7c488f9e473f51a4becf76e8a
Iman <imangatine@gmail.com>
2017-07-20 12:05:07 +0200

    creation route

--- app/index.js
+++ app/index.js
@@ -1,4 +1,48 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+//const IndexCtrl = require('./controllers/IndexCtrl');
+
+const config = {
+    name: "Bibi"
+};
+
+const port = 3000;
 const app = express();
-app.listen(3000, () => console.log("ready
+
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => res.send(`Hello World inconnu!`));
+
+// Sans vue
+app.use(express.static(path.join(__dirname, '..', 'public')));
+//     res.send(`Hello World ${name}!`);
+// });
+
+//const indexCtrl = new IndexCtrl(config);
+
+// Avec une vue
+app.get('/:name?', (req, res) => {
+    const name = req.params.name || 'inconnu';
+
+//app.get('/:name?', indexCtrl.index.bind(indexCtrl));
+app.get('/registration', (req, res) => {});
+app.get('/login', (req, res) => {});
+app.get('/generator', (req, res) => {});
+app.get('/download', (req, res) => {});
+app.post('/postRegistration', (req, res) => {});
+app.post('/postLogin', (req, res) => {});
+app.post('/postGenerator', (req, res) => {});
+
+res.render('index', {
+    name
+});
+});
+//app.post('/auth', indexCtrl.auth.bind(indexCtrl));
+
+app.listen(port, () => console.log('Connection ready on : ${port}')); 
